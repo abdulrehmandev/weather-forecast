b614308eade420ae73c850b518dbf78ba0d0b284
Abdul Rehman <beingabdulr@gmail.com>
2022-12-22 17:21:19 +0500

    Add Weather-Forecast files

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import SearchForm from "./components/SearchForm";
+
+function App() {
+	return (
+		<div className="App">
+			<SearchForm />
+		</div>
+	);
+}
+
+export default App;
